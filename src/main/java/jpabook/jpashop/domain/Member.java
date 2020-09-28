2d9e0d8f2ad57707e1bdce9b28feae4b8d177750
yosepjeon <enekelx1@naver.com>
2020-09-28 18:43:01 +0900

    연관관계 매핑 시작

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Setter;
 import lombok.ToString;
@@ -22,4 +26,8 @@ public class Member {
 	private String city;
 	private String street;
 	private String zipcode;
+	
+	// 설계적으로는 잘못됨 학습용으로 이렇게 설계함.
+	@OneToMany(mappedBy="member")
+	private List<Order> orders = new ArrayList<Order>();
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +10,9 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -15,14 +20,26 @@ import javax.persistence.Table;
 public class Order {
 	@Id
 	@GeneratedValue
-	@Column(name="order_id")
+	@Column(name="ORDER_ID")
 	private Long id;
 	
-	@Column(name="member_id")
-	private Long memberId;
+//	@Column(name="member_id")
+//	private Long memberId;
+	
+	@ManyToOne
+	@JoinColumn(name="MEMBER_ID")
+	private Member member;
+	
+	@OneToMany(mappedBy = "order")
+	private List<OrderItem> orderItems = new ArrayList<OrderItem>();
 	
 	private LocalDateTime orderDate;
 	
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status;
+	
+	public void addOrderItem(OrderItem orderItem) {
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
 }
